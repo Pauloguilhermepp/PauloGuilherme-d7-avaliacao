27f2e7012a0718431bbe5ebd922c96d1848a63dd
Paulo Guilherme <pauloguilherme@usp.br>
2022-10-02 14:32:10 -0300

    Fix database communication and improve UI

--- PauloGuilherme-d7-avaliacao/PauloGuilherme-d7-avaliacao/MainWindow.xaml.cs
+++ PauloGuilherme-d7-avaliacao/PauloGuilherme-d7-avaliacao/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using System.Windows;
 using PauloGuilherme_d7_avaliacao.Data;
-using System.ComponentModel.DataAnnotations;
 
 namespace PauloGuilherme_d7_avaliacao;
 
@@ -22,34 +21,36 @@ public partial class MainWindow : Window
 
     public void AccessSystem(object s, RoutedEventArgs e)
     {
-        if (checkIfEmailIsValid(possibleUser.Email))
+
+        if (validLoginAttempt())
         {
+
             var users = context.Users.ToList();
             foreach (User user in users)
             {
-                if (user.Email == possibleUser.Email && user.Password == possibleUser.Password)
+                if (checkEmailAndPassword(user))
                 {
-                    MessageBox.Show("Usuário autentificado");
+                    MessageBox.Show("Usuário autentificado!");
                     return;
-                } 
+                }
             }
         }
 
-        MessageBox.Show("Credenciais inválidas");
+        MessageBox.Show("Credenciais inválidas!");
     }
 
-    public static bool checkIfEmailIsValid(string email)
+    private bool validLoginAttempt()
     {
-        if (email == null)
-        {
-            return false;
-        }
+        return isValid(possibleUser.Email) && isValid(possibleUser.Password);
+    }
 
-        if (new EmailAddressAttribute().IsValid(email))
-        {
-            return true;
-        }
+    private static bool isValid(string field)
+    {
+        return (field != null && field.Length != 0);
+    }
 
-        return false;
+    private bool checkEmailAndPassword(User user)
+    {
+        return (user.Email == possibleUser.Email && possibleUser.Password == user.Password);
     }
 }
